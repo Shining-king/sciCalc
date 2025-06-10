b777765e6528578c32549e9f31e8189b8e6ee8f1
Nicolas lobo <nic.loboni@gmail.com>
2025-06-10 23:09:13 +0200

    button

--- scicalc/lib/screens/calc_screen.dart
+++ scicalc/lib/screens/calc_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class calcScreen extends StatelessWidget {
+class calcScreen extends StatefulWidget {
   const calcScreen({Key? key}) : super(key: key);
+
+  @override
+  State<calcScreen> createState() => _calcScreenState();
+}
+
+class _calcScreenState extends State<calcScreen> {
+  Widget _button(String text, VoidCallback onPressed) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Expanded(
+        child: ElevatedButton(onPressed: onPressed, child: Text(text)),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +29,25 @@ class calcScreen extends StatelessWidget {
         children: [
           Padding(
             padding: EdgeInsets.all(14),
-            child: Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.yellow.shade100,
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 100),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow.shade100,
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 100),
+                ),
+                Row(
+                  children: [
+                    _button('7', () {}),
+                    _button('8', () {}),
+                    _button('9', () {}),
+                    _button('/', () {}),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
